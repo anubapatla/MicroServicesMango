d266fc59693d4603a05865c0ba3cfa2cea8b4d0f
AnuBapatla <anusrichatala@gmail.com>
2026-01-03 23:40:44 -0500

    new controller created

---
+++ Mango.Services.CouponAPi/Controllers/CouponAPIController.cs
@@ -0,0 +1,31 @@
+﻿using Mango.Services.CouponAPi.Data;
+using Mango.Services.CouponAPi.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+
+namespace Mango.Services.CouponAPi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CouponAPIController : ControllerBase
+    {
+        private readonly AppDbContext _db;
+        public CouponAPIController( AppDbContext db)
+        {
+            _db = db;
+        }
+        [HttpGet]
+        public object Get()
+        {
+            try
+            {
+                IEnumerable<Coupon> objList = _db.Coupons.ToList();
+                return objList;
+            }catch(Exception ex)
+            {
+
+            }return null;
+        }
+    }
+}
