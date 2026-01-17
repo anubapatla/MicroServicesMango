ae8b08c2e411ccadc67e2ef2cb78b64401f5a2ed
AnuBapatla <anusrichatala@gmail.com>
2026-01-17 00:24:33 -0500

    couponcode view created

--- Mango.Web/Controllers/CouponController.cs
+++ Mango.Web/Controllers/CouponController.cs
@@ -27,5 +27,9 @@ namespace Mango.Web.Controllers
        
             return View(list);
         }
+        public async Task<IActionResult> CouponCreate()
+        {
+            return View();
+        }
     }
 }
