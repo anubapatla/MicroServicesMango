9ecbff02f90144ba9f49656e03c12f973f6631b0
AnuBapatla <anusrichatala@gmail.com>
2026-01-31 19:16:37 -0500

    migrated identity table

--- mango.Services.AuthAPI/Data/AppDbContext.cs
+++ mango.Services.AuthAPI/Data/AppDbContext.cs
@@ -1,33 +1,19 @@
-﻿using Mango.Services.CouponAPi.Model;
+﻿
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace Mango.Services.CouponAPi.Data
+namespace mango.Services.AuthAPI.Data
 {
-    public class AppDbContext:DbContext
+    public class AppDbContext : IdentityDbContext<IdentityUser>
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options):base (options)
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
-            
-        }
-        public DbSet<Coupon>Coupons { get; set; }
 
+        }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
-            modelBuilder.Entity<Coupon>().HasData(new Coupon
-            {
-                CouponId = 1,
-                CouponCode = "10OFF",
-                DiscountAmount=10,
-                MinAmount=20
-            });
-            modelBuilder.Entity<Coupon>().HasData(new Coupon
-            {
-                CouponId = 20,
-                CouponCode = "20OFF",
-                DiscountAmount = 20,
-                MinAmount = 40
-            });
         }
     }
 }
