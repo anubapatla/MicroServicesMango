94922ded71af9e9be518c534efdd665def1df940
AnuBapatla <anusrichatala@gmail.com>
2026-02-13 23:51:16 -0500

    create IAuthService in web project

---
+++ Mango.Web/Services/IService/IAuthService.cs
@@ -0,0 +1,12 @@
+﻿using mango.web.Models;
+using Mango.Web.Models;
+
+namespace Mango.Web.Services.IService
+{
+    public interface IAuthService
+    {
+        Task<ResponseDto?> LoginAsync(LoginRequestDto loginRequestDto);
+        Task<ResponseDto?> RegisterAsync(LoginRequestDto loginRequestDto);
+        Task<ResponseDto?> AssignRoleAsync(RegistrationRequestDto registrationRequestDto)
+    }
+}
